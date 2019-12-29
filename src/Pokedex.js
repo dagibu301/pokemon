a0d866fb4290da5b0fdcc604533fe2c2e590780d
David Giraldo <davidgiraldobueno@gmail.com>
2019-12-28 21:11:29 -0500

    pokecard

---
+++ src/Pokedex.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Pokecard from "./Pokecard";
+
+class Pokedex extends Component {
+  render() {
+    return (
+    <div>
+        
+
+    </div>
+    );
+  }
+}
+
+export default Pokedex;

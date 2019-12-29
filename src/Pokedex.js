b5a844f6b912f57c17bc9c06d0cff109d3a917bc
David Giraldo <davidgiraldobueno@gmail.com>
2019-12-28 21:41:32 -0500

    some styles

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react";
 import Pokecard from "./Pokecard";
-import pokemons from "./pokemons"
+import pokemons from "./pokemons";
+import "./Pokedex.css"
 
 class Pokedex extends Component {
-    static defaultProps = {
-
-    }
+  static defaultProps = {};
   render() {
     return (
-    <div className="Pokedex">
+      <div className="Pokedex">
         <h1>Pokedex!</h1>
-        {pokemons.map((p) => (
-            <Pokecard key={p.id} id={p.id} name={p.name} type={p.type} exp={p.base_experience}  />
-            ) ) }
-
-    </div>
+        <div className="Pokedex-cards">
+          {pokemons.map(p => (
+            <Pokecard
+              key={p.id}
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
+      </div>
     );
   }
 }

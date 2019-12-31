31c6edeb6115d580dbf1e1042fabb19cbfd83b3f
David Giraldo <davidgiraldobueno@gmail.com>
2019-12-31 15:46:02 -0500

    Updates

---
+++ precache-manifest.918b98200bf245452d1205f599577e7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e213263e9698f921a3933c7c3fe65af2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f23e05ae19056ec8146a",
+    "url": "/static/css/main.dabbe5a6.chunk.css"
+  },
+  {
+    "revision": "6b1635b783986b07f9ed",
+    "url": "/static/js/2.3f4ba9e3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3f4ba9e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f23e05ae19056ec8146a",
+    "url": "/static/js/main.ae3aaa6b.chunk.js"
+  },
+  {
+    "revision": "2a5e3384565004702838",
+    "url": "/static/js/runtime-main.b99d0e78.js"
+  }
+]);

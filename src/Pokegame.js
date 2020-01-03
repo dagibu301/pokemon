ddb8d2fb1d36e13e5b27f53bba7c3642fae9ddd9
David Giraldo <davidgiraldobueno@gmail.com>
2020-01-02 21:03:13 -0500

    button reload

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 import pokemons from "./pokemons";
-import Pokedex from './Pokedex';
+import Pokedex from "./Pokedex";
+import "./Pokegame.css"
 
 class Pokegame extends Component {
-    render(){
-
-        let hand1 = [];
-        let hand2 = [ ...pokemons ];
-        while(hand1.length < hand2.length){
-            let randIdx = Math.floor(Math.random() * hand2.length );
-            let randPokemon = hand2.splice(randIdx, 1)[0];
-            hand1.push(randPokemon);
-        }
-
-        let exp1 = hand1.reduce( (exp,pokemon) => exp + pokemon.base_experience, 0 );
-        let exp2 = hand2.reduce( (exp,pokemon) => exp + pokemon.base_experience, 0 );
-        return (
-            <div>
-                <Pokedex pokemon={hand1} exp={exp1}  isWinner= { exp1 > exp2}/>
-                <Pokedex pokemon={hand2} exp={exp2}  isWinner= { exp2 > exp1}/>
-            </div>
-        );
+  constructor(props) {
+    super(props);
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+  clickHandler(e) {
+    console.log("Force update");
+    this.forceUpdate();
+  }
+  render() {
+    let hand1 = [];
+    let hand2 = [...pokemons];
+    while (hand1.length < hand2.length) {
+      let randIdx = Math.floor(Math.random() * hand2.length);
+      let randPokemon = hand2.splice(randIdx, 1)[0];
+      hand1.push(randPokemon);
     }
+
+    let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+    let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+    return (
+      <div className="Pokegame">
+        <Pokedex pokemon={hand1} exp={exp1} isWinner={exp1 > exp2} />
+        <div className="Pokegame-button-container">
+          <button className="Pokegame-button" onClick={this.clickHandler}>
+            Restart!
+          </button>
+        </div>
+
+        <Pokedex pokemon={hand2} exp={exp2} isWinner={exp2 > exp1} />
+      </div>
+    );
+  }
 }
 
 export default Pokegame;
